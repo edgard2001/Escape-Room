cda0e30416f7477d24ea052beea9fe54fba7bbd7
Edgard Romanovskiy <eromanovskiy01@qub.ac.uk>
2021-05-28 12:43:08 +0100

    Fixed Object folder file path

--- js/torchRoom.js
+++ js/torchRoom.js
@@ -1,4 +1,4 @@
-function moveTorch(event){
-  var torch = document.getElementsByClassName("torch")[0];
-  torch.style.clipPath = `circle(80px at ${event.offsetX}px ${event.offsetY}px)`;
+function moveTorch(event) {
+    var torch = document.getElementsByClassName("torch")[0];
+    torch.style.clipPath = `circle(80px at ${event.offsetX}px ${event.offsetY}px)`;
 }
